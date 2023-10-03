cfa1d06569698b20f076f84250c21f2f998746fb
Vladislav <vladislavzikstudy@gmail.com>
2023-10-03 17:04:37 +0300

    Added __create_nodes_layers method Added show methods refactor

--- visualizer.py
+++ visualizer.py
@@ -1,27 +1,46 @@
 import requests
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class MeshGraph:
     def __init__(self):
         self.__points = []
+        self.__graph = None
 
-    def create_points_from_maps(self, main_node: dict) -> list:
+    def create_edges(self, main_node: dict) -> list:
         main_node_id = main_node.get('nodeId')
         if main_node_id:
             subs = main_node.get('subs')
             if subs:
                 for sub in subs:
                     self.__points.append((main_node_id, sub['nodeId']))
-                    self.create_points_from_maps(sub)
+                    self.create_edges(sub)
 
         return self.__points
+
+    def __create_nodes_layers(self):
+        for layer, nodes in enumerate(nx.topological_generations(self.__graph)):
+            for node in nodes:
+                print(node, layer)
+                self.__graph.nodes[node]["layer"] = layer
+
+    def show(self, title='MESH BSUIR 1-339'):
+        self.__graph = nx.DiGraph(self.__points)
+        self.__create_nodes_layers()
+        pos = nx.multipartite_layout(self.__graph, subset_key="layer")
+        fig, ax = plt.subplots()
+        nx.draw_networkx(self.__graph, pos=pos, ax=ax, node_size=4000, font_size=10)
+        ax.set_title(title)
+        fig.tight_layout()
+        plt.show()
 
 
 class MeshDevice:
     def __init__(self, url: str):
         self.__url = url
 
-    def get_mesh_map(self) -> dict:
+    def scan_network(self) -> dict:
         response = requests.get(self.__url)
         return response.json()
 
@@ -29,6 +48,7 @@ class MeshDevice:
 # data = '{"nodeId":474950769,"root":true,"subs":[{"nodeId":679054265,"subs":[{"nodeId":699587809},{"nodeId":675614937,"subs":[{"nodeId":699587633,"subs":[{"nodeId":697631273,"subs":[{"nodeId":697627909}]}]}]}]}]}'
 link = 'http://192.168.0.102:80/scan/'
 root_device = MeshDevice(link)
-data = root_device.get_mesh_map()
+data = root_device.scan_network()
 graph = MeshGraph()
-print(graph.create_points_from_maps(data))
+graph.create_edges(data)
+graph.show()
