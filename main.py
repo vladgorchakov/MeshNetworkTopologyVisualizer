f24df80ea9f16994fd41a7130576f320c90b607c
Vladislav <vladislavzikstudy@gmail.com>
2023-10-05 12:41:11 +0300

    Add check data for main

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ def main():
     link = 'http://192.168.0.102:80/scan/'
     root_device = MeshNode(link)
     data = root_device.scan_network()
-    graph = MeshGraph()
-    graph.create_edges(data)
-    graph.show()
+    if data:
+        graph = MeshGraph()
+        graph.create_edges(data)
+        graph.show()
 
 
 if __name__ == "__main__":
