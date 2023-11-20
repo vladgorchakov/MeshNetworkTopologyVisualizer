f44c40378265cd4190be9b0a978a8227fc062a01
Vladislav <vladislavzikstudy@gmail.com>
2023-11-20 18:11:36 +0300

    add msg coder

--- coder.py
+++ coder.py
@@ -8,13 +8,14 @@ size = 250
 error_counter = 0
 
 msg_tx = [randint(0, 1) for i in range(size)]
-print(f"payload len: {len(msg_tx)}")
+print(f"msg_tx len: {len(msg_tx)}")
 
 if len(msg_tx) % k:
     d = (k - size % k)
     msg_tx += d * [0]
-    print(f"added bits: {d}")
+    print(f"added bits to msg_tx: {d}")
 
+# Дополнение до деления на целое значение
 msg_to_encode = []
 buf = []
 for i in msg_tx:
@@ -23,20 +24,19 @@ for i in msg_tx:
         msg_to_encode.append(buf)
         buf = []
 
-print(f"msg size: {len(msg_to_encode)} x {len(msg_to_encode[0])}")
+print(f"msg_to_encode size: {len(msg_to_encode)} x {len(msg_to_encode[0])} = ", end='')
+print(f"{len(msg_to_encode) * len(msg_to_encode[0])} bits")
 
-msg_decode = msg_to_encode
-msg_rx = [byte for message in msg_decode for byte in message]
-print(msg_rx)
-print(len(msg_rx))
-msg_rx = msg_rx[:-3]
-print(len(msg_rx))
+msg_decode = msg_to_encode # получили то, что отправили
+msg_rx = [byte for message in msg_decode for byte in message] # одно большое сообщение
+msg_rx = msg_rx[:-3] # удаление дополнительных битов
 
-msg_rx[45] = 0 if (msg_rx[45] == 1) else 1
+
+msg_rx[45] = 0 if (msg_rx[45] == 1) else 1 # внесение ошибки в код
 for i in range(len(msg_rx)):
     if msg_tx[i] != msg_rx[i]:
         error_counter += 1
-        print(f'{i}: {msg_tx[i]} : {msg_rx[i]}')
-    # print(f'{i}: {msg[i]} : {msg_rx[i]}')
+        print(f'error index: {i}:')
+        print(f"msg_tx[{i}] : {msg_tx[i]}; msg_rx[{i}] {msg_rx[i]}")
 
 print(f'error counter: {error_counter}')
